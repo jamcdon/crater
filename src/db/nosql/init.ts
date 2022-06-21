eee1a08db10aac0e28c11b18ad350a914601c55f
jared <jaredamcdon@gmail.com>
2022-06-20 23:31:11 -0500

    changed mongo uri string

--- src/db/nosql/init.ts
+++ src/db/nosql/init.ts
@@ -6,7 +6,8 @@ const dbUser = process.env.DB_USER as string;
 const dbHost = process.env.NO_SQL_HOST as string;
 const dbPassword = process.env.DB_PASSWORD as string;
 
-const connectionString: string = `mongodb://${dbUser}:${dbPassword}@${dbHost}:27017/${dbName}`
+//const connectionString: string = `mongodb://${dbUser}:${dbPassword}@${dbHost}:27017/${dbName}`
+const connectionString: string = `mongodb://${dbHost}:27017/${dbName}`
 const noSqlInit = () => {
     mongoose.connect(connectionString)
     console.log('Connected to NOSQL databse.')
