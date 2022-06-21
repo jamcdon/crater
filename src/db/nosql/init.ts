3c3f74f049520cb4c3616ede01a560d652123c1f
jared <jaredamcdon@gmail.com>
2022-06-21 13:25:55 -0500

    adding frontend and updating packages

--- src/db/nosql/init.ts
+++ src/db/nosql/init.ts
@@ -2,15 +2,12 @@ import mongoose from 'mongoose'
 import { ConnectOptions } from 'mongoose';
 
 const dbName = process.env.DB_NAME as string;
-const dbUser = process.env.DB_USER as string;
 const dbHost = process.env.NO_SQL_HOST as string;
-const dbPassword = process.env.DB_PASSWORD as string;
 
-//const connectionString: string = `mongodb://${dbUser}:${dbPassword}@${dbHost}:27017/${dbName}`
 const connectionString: string = `mongodb://${dbHost}:27017/${dbName}`
 const noSqlInit = () => {
     mongoose.connect(connectionString)
-    console.log('Connected to NOSQL databse.')
+    console.log('Connecting to NOSQL database...')
 }
 
 export default noSqlInit
--- src/db/sql/init.ts
+++ src/db/sql/init.ts
@@ -2,7 +2,7 @@ import { User } from './models';
 
 const sqlInit = () => {
     User.sync()
-    console.log('Connected to SQL database.')
+    console.log('Connecting to SQL database...')
 }
 
 export default sqlInit;
