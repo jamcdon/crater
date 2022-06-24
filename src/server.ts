7e02d7eb3c999e6643f646982ea57b37171266e4
jared <jaredamcdon@gmail.com>
2022-06-23 23:03:08 -0500

    added static path directory

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,7 @@ const PORT = 3000;
 
 //pug middleware
 app.set("views", path.join(__dirname, "../views"))
+app.use(express.static(path.join(__dirname, "../public")))
 app.set("views engine", "pug")
 
 //Body parsing middleware
