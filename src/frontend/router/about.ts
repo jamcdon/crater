39e26e1cb0b0e14a41d18b10015e7296f3c238d7
jared <jaredamcdon@gmail.com>
2022-06-24 19:00:45 -0500

    unsaved change => saved

--- src/frontend/router/about.ts
+++ src/frontend/router/about.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express'
-import { aboutController } from '../../controllers'
+import { aboutController } from '../controllers'
 
 const aboutRouter = Router()
 
