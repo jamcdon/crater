d65ee10eab721aa2d6cd19db5f8f832dbfbc50e5
jared <jaredamcdon@gmail.com>
2022-07-04 14:30:17 -0500

    further connection string changes

--- src/db/nosql/init.ts
+++ src/db/nosql/init.ts
@@ -7,7 +7,8 @@ const dbPassword = process.env.DB_PASSWORD as string;
 const dbHost = process.env.NO_SQL_HOST as string;
 
 //const connectionString: string = `mongodb://${dbHost}:27017/${dbName}`
-const connectionString: string = `mongodb://${dbUser}:${dbPassword}@${dbHost}:27017/${dbName}?authSource=admin&w=1`
+//const connectionString: string = `mongodb://${dbUser}:${dbPassword}@${dbHost}:27017/${dbName}?authSource=admin&w=1`
+const connectionString: string = `mongodb://${dbUser}:${dbPassword}@${dbHost}:27017/`
 const noSqlInit = () => {
     mongoose.connect(connectionString)
     console.log('Connecting to NOSQL database...')
