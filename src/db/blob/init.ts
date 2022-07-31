19d9474dd5eea604c975b35f82f62fdee0afe5b1
jared <jaredamcdon@gmail.com>
2022-07-31 11:57:02 -0500

    changed await syntax

--- src/db/blob/init.ts
+++ src/db/blob/init.ts
@@ -10,14 +10,13 @@ export default async function blobInit(): Promise<boolean> {
     let successes: Array<boolean> = []
     
     for (let bucket of buckets) {
-        let policyInit: boolean | undefined
-
-        await bucket.createIfNotExists()
-        setTimeout(() => {}, 8000)
-        await bucket.setPoicyRO()
-        setTimeout(() => {}, 8000)
-        const policySet = await bucket.validatePolicy()
-        successes.push(policySet)
+        await bucket.createIfNotExists().then(async () => {
+            setTimeout(() => {}, 8000)
+            await bucket.setPoicyRO()
+            setTimeout(() => {}, 8000)
+            const policySet = await bucket.validatePolicy()
+            successes.push(policySet)
+        })
     }
     if(successes.includes(false)){
         return false
