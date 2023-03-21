fa30c3130e97a0c376885614bbaaca7d065e1901
jared <jaredamcdon@gmail.com>
2023-03-21 12:19:12 -0400

    changing to networkidle2 for increased efficiency WHILE slighlty higher reliability

--- src/db/blob/services/imageScraper.ts
+++ src/db/blob/services/imageScraper.ts
@@ -18,7 +18,7 @@ const puppetPage = async (url: string): Promise<[JSDOM, Browser] | [undefined, u
     })
 
     const page = await browser.newPage()
-    await page.goto(url, {waitUntil: 'domcontentloaded'})
+    await page.goto(url, {waitUntil: 'networkidle2'})
 
     const bodyHandle = await page.$('body')
     const html = await page.evaluate(body => body?.innerHTML, bodyHandle)
